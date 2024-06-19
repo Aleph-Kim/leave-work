662719abe457ec2d987c5e60dd377aecf62ddb8e
김채민 <dktjdej25@gmail.com>
2024-06-19 17:19:26 +0900

    First Commit

---
+++ src/main/java/org/example/leavework/LeaveWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.leavework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveWorkApplication.class, args);
+    }
+
+}
